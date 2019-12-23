d41f7bd456c978111583c4be9ec0a5094bec0900
西木 <@192.168.1.120>
2019-12-23 22:27:59 +0800

    开发用户模块

--- src/com/bigdata/UserController.java
+++ src/com/bigdata/UserController.java
@@ -4,6 +4,7 @@ public class UserController {
 	public static void main(String[] args) {
 		System.out.println("hehehehe");
 		System.out.println("hello");
+		System.out.println("hhhhhhhh");
 	}
 
 }
