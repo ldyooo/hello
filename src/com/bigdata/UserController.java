be647f8e227293a0c6071ba3a1b5d7ff75057b71
西木 <@192.168.1.120>
2019-12-23 22:58:32 +0800

    修改bug

--- src/com/bigdata/UserController.java
+++ src/com/bigdata/UserController.java
@@ -4,6 +4,7 @@ public class UserController {
 	public static void main(String[] args) {
 		System.out.println("hehehehe");
 		System.out.println("hahahahhaha");
+		System.out.println("666666");
 		System.out.println("hhhhhhhh");
 	}
 
