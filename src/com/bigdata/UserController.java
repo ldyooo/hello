c3572ec908a32e338f90da4eea7c347bb8714239
西木 <@192.168.1.120>
2019-12-23 22:44:02 +0800

    第二次提交

--- src/com/bigdata/UserController.java
+++ src/com/bigdata/UserController.java
@@ -3,7 +3,7 @@ package com.bigdata;
 public class UserController {
 	public static void main(String[] args) {
 		System.out.println("hehehehe");
-		System.out.println("hello");
+		System.out.println("hahaha");
 		System.out.println("hhhhhhhh");
 	}
 
