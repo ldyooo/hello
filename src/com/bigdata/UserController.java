c1bf035a3703336c0dc3697cf35ea3779b4e1602
西木 <@LAPTOP-4VM9SFV6>
2019-12-24 12:16:16 +0800

    提交

--- src/com/bigdata/UserController.java
+++ src/com/bigdata/UserController.java
@@ -2,17 +2,11 @@ package com.bigdata;
 
 public class UserController {
 	public static void main(String[] args) {
+		System.out.println("hehehehe");	
+		System.out.println("aaaaaaaaaaaa");
 		System.out.println("hehehehe");
-		System.out.println("hahahahhaha");
-		System.out.println("666666");
-		System.out.println("hhhhhhhh");
-		System.out.println("1111111111");
-		System.out.println("sssssssssssssssss");
-		System.out.println("cccccccc");
+		System.out.println("hhhhhhhhh");
 
-		System.out.println("1111111111");
-		System.out.println("uuuuuuuuuuuuuuuu");
-		System.out.println("最最最最最最最");
 	}
 
 } 
